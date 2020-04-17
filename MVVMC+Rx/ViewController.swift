960648736d9d4648260812e69eab55faea8e4a30
andrew54068 <andrew0424718012@yahoo.com.tw>
2020-04-17 13:30:23 +0800

    Initial Commit

---
+++ MVVMC+Rx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMC+Rx
+//
+//  Created by kidnapper on 2020/4/17.
+//  Copyright © 2020 andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
